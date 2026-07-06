12799c63ec96aff931aaf5e56dbddff631e44231
shiblo-dev <devshiblo6@gmail.com>
2026-07-06 23:59:23 +0600

    project set up and all npm install done

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express,{ Application, Request, Response } from "express";
+import config from "./config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app : Application = express();
+
+app.use(cors({
+    origin : config.app_url,
+    credentials : true,
+}))
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(cookieParser());
+
+
+app.get("/",(req : Request, res : Response) => {
+    res.send("Hello, World!");
+});
+
+ 
+
+export default app;
