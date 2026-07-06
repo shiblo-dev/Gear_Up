2663641a2c19075db6b8da924a07ee779779a79b
shiblo-dev <devshiblo6@gmail.com>
2026-07-07 00:04:43 +0600

    catchAsync and sendResponse created

---
+++ src/utils/sendResponse.ts
@@ -0,0 +1,17 @@
+import { Response } from "express";
+
+type TResponseData<T> = {
+    success: boolean;
+    statusCode: number;
+    message: string;
+    data: T;
+
+}
+export const sendResponse = <T>(res: Response, data: TResponseData<T>) => {
+    res.status(data.statusCode).json({
+        success: data.success,
+        statusCode: data.statusCode,
+        message: data.message,
+        data: data.data
+    })
+}
